cad269bb6a461f2fea476dcc8a279190dcd4b301
Pedro Franchi <pedro.fjesus@sptech.school>
2025-04-01 09:04:41 -0300

    feat: :sparkles: Criando componente de página de login

---
+++ caringu-frontend/src/components/Login/ColunaImagem.jsx
@@ -0,0 +1,22 @@
+import './login.css'; // Importando o CSS
+import { Link } from 'react-router-dom'; // Importando o Link
+import logo from '../../assets/logos/caringu-logotipo-light.svg';
+import imagemLogin from '../../assets/images/imagem-login.svg';
+
+
+const ColunaImagem = () => {
+  return (
+    <>
+
+        <section className="coluna1">
+          <Link to="/teste">
+            <img className="logo" src={logo} alt="Logo da CaringU" />
+          </Link>
+          <img className="imagem-login" src={imagemLogin} />
+        </section>
+    
+    </>
+  );
+};
+
+export default ColunaImagem;
