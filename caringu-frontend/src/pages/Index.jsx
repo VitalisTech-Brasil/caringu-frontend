96a412a59946950e10e0145ba3203cf80365e143
Pedro <pedro.fjesus@sptech.school>
2025-04-03 13:01:09 -0300

    feat: :fire: Tela index

---
+++ caringu-frontend/src/pages/Index.jsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+
+const Index = () => {
+    return (
+      <>
+      <div>
+          <Link to='./Login'>
+            <h1><b>LOGIN</b></h1>
+          </Link>
+      </div>
+      </>
+    );
+  };
+  
+  export default Index;
