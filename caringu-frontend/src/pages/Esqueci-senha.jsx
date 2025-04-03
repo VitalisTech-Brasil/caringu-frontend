6376b481d861b56dfb1990cdb893974cc5b038cf
Pedro Franchi <pedro.fjesus@sptech.school>
2025-04-03 14:57:38 -0300

    feat: :sparkles: Iniciando tela esqueci a senha

--- caringu-frontend/src/pages/Esqueci-senha.jsx
+++ caringu-frontend/src/pages/Esqueci-senha.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
-import '../styles/esqueciSenha.css'
+// import '../styles/esqueciSenha.css'
 import imagemEsqueciSenha from '../assets/images/imagem-esqueci-senha.svg'
 
 
 const EsqueciSenha = () => {
     return (
         <>
-            <main className='flex-row gap-10 h-screen w-screen'>
-                <section className='w-1/2'>
-                    <div className='flex justify-center items-center bg-black h-218 w-full'>Teste</div>
+            <main className='flex-row h-screen w-screen'>
+                <section className='flex justify-center items-center h-screen w-1/2'>
+                    <div className='flex justify-center items-center h-full w-full'>
+                        <div className='flex justify-center items-center w-7 h-100 flex-col'>
+                            <div className='flex w-100 items-center justify-between'>
+                                <div className='border-2 border-red-950'>quadrado 1</div>
+                                <div className='border-black border-10'>quadrado 2</div>
+                                <div className='border-black border-10'>quadrado 3</div>
+                            </div>
+                            <div className='bg-green-900 text-blue-900'>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</div>
+                            <div className='bg-amber-950 text-emerald-500'>WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW</div>
+                        </div>
+                    </div>
                 </section>
-                <section className="flex justify-center items-center w-1/2">
-                    <img className="h-218 w-full" src={imagemEsqueciSenha} />
+                <section className='h-screen w-1/2'>
+                    <div className="flex justify-end h-full w-full">
+                        <img src={imagemEsqueciSenha} alt="" />
+                    </div>
                 </section>
             </main>
         </>
