f33fe25ed5472ee551daca9348e7334da6512e5e
Pedro <pedro.fjesus@sptech.school>
2025-04-07 21:42:37 -0300

    feat: :sparkles: Inicio da tela principal

--- caringu-frontend/src/pages/Index.jsx
+++ caringu-frontend/src/pages/Index.jsx
@@ -1,21 +1,31 @@
 import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import logoBranco from '../assets/logos/caringu-logotipo-branco.svg'
 
 const Index = () => {
 
-    useEffect(() => {
-      document.title = "CaringU"
-    }, []);
+  useEffect(() => {
+    document.title = "CaringU"
+  }, []);
 
-    return (
-      <>
-      <div>
-          <Link to='./Login'>
-            <h1><b>LOGIN</b></h1>
-          </Link>
-      </div>
-      </>
-    );
-  };
-  
-  export default Index;
+  return (
+    <>
+      <header className='bg-black w-screen h-27 flex justify-between items-center text-white'>
+        <div className=''>
+          <ul className='flex flex-row justify-between w-100'>
+            <li>Home</li>
+            <li>Sobre Nós</li>
+            <li>Serviços</li>
+            <li>Fale Conosco</li>
+          </ul>
+        </div>
+        <div>
+          <img src={logoBranco} alt="" />
+        </div>
+        
+      </header>
+    </>
+  );
+};
+
+export default Index;
