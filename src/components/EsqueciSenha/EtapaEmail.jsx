2df57433dc57e08542dfc192a9a3c3779470810f
Pedro Franchi <pedro.fjesus@sptech.school>
2025-04-10 14:54:53 -0300

    feat: Iniciando tela principal

---
+++ src/components/EsqueciSenha/EtapaEmail.jsx
@@ -0,0 +1,61 @@
+// src/components/EsqueciSenha/EtapaEmail.jsx
+import React from 'react';
+import Input from '../Login/inputs';
+
+const EtapaEmail = ({ email, setEmail, onAvancar }) => {
+  const handleEnviarEmail = async () => {
+    try {
+      const response = await fetch("url do servidor pra ver se existe o email", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email }),
+      });
+
+      const data = await response.json();
+      if (data.success) {
+        onAvancar();
+      } else {
+        alert("Email não encontrado.");
+      }
+    } catch (error) {
+      console.error("Erro ao enviar e-mail:", error);
+    }
+  };
+
+  return (
+    <section className='flex justify-center items-center h-screen w-1/2'>
+      <div className='flex justify-center items-center h-full w-full'>
+        <div className='flex justify-center items-center w-full h-150 flex-col gap-10'>
+          <div className='flex w-100 items-center justify-between'>
+            <div className='bg-[var(--cor-primaria)] rounded-full h-6 w-25'></div>
+            <div className='bg-[var(--azul-claro)]  rounded-full h-6 w-25'></div>
+            <div className='bg-[var(--azul-claro)]  rounded-full h-6 w-25'></div>
+          </div>
+          <div className='text-[var(--cor-primaria)] h-27 w-1/2 text-center flex-col justify-end'>
+            <h1 className=' text-[48px]'>Recuperação de senha</h1>
+            <p>Não se preocupe! Isso acontece. Informe seu e-mail e enviaremos um link para você redefinir sua senha.</p>
+          </div>
+          <div className='w-1/2'>
+            <Input
+              id="email"
+              name="email"
+              type="email"
+              label="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <footer className='flex flex-col h-25 justify-between items-center'>
+            <button onClick={handleEnviarEmail} className='w-[511px] h-[50px] bg-[var(--laranja)] text-[var(--cor-secundaria)] rounded-[8px]'>
+              Resetar Senha
+            </button>
+            <a href="/Login">Voltar para Login</a>
+          </footer>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default EtapaEmail;
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,21 +1,44 @@
-import { useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import bgImage from "../assets/images/primeira-imagem-fundo-index.svg";
+import logoImage from '../assets/logos/caringu-logo-branco.svg'
 
-const Index = () => {
-
-    useEffect(() => {
-      document.title = "CaringU"
-    }, []);
-
-    return (
-      <>
-      <div>
-          <Link to='./Login'>
-            <h1><b>LOGIN</b></h1>
-          </Link>
-      </div>
-      </>
-    );
-  };
-  
-  export default Index;
+export default function HomePage() {
+  return (
+    <>
+      <header className="absolute top-0 left-0 z-1 w-full flex items-center justify-between text-white px-8">
+        <div className="m-3 flex justify-center items-center bg-[#15171B]  h-[50px] w-[450px] ">
+          <nav className="flex gap-10">
+            <a href="#" className="text-sm hover:underline">Home</a>
+            <a href="#" className="text-sm hover:underline">Sobre nós</a>
+            <a href="#" className="text-sm hover:underline">Serviços</a>
+            <a href="#" className="text-sm hover:underline">Fale conosco</a>
+          </nav>
+        </div>
+        <img src={logoImage} alt="Logo CaringU" className="h-10" />
+        <div className="flex gap-4 items-center">
+          <a href="#" className="text-sm hover:underline">Inscreva-se</a>
+          <button className="bg-[var(--laranja)] text-white text-sm font-medium px-4 py-2 rounded hover:bg-orange-600 transition-all">
+            Entrar
+          </button>
+        </div>
+      </header>
+        <section className="relative w-full h-screen bg-cover flex items-start" style={{ backgroundImage: `url(${bgImage})` }}>
+          <div className="absolute inset-0 bg-black opacity-60" />
+          <div className="relative z-10 flex flex-col justify-center items-start h-full text-white max-w-4xl mx-25">
+            <h1 className="text-4xl md:text-5xl font-extrabold mb-6">
+              Conquiste alunos com mais facilidade e praticidade
+            </h1>
+            <p className="text-lg mb-8 max-w-xl">
+              A CaringU facilita a gestão, conecta você a novos alunos e otimiza a comunicação, permitindo focar no que importa: Transformar vidas através do treino.
+            </p>
+            <button className="bg-[var(--laranja)] text-white font-semibold px-6 py-3 rounded hover:bg-orange-600 transition">
+              Cadastre-se
+            </button>
+          </div>
+        </section>
+        <section className="w-[98%] h-screen">
+          asda
+        </section>
+    </>
+  );
+}
