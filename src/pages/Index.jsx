c9d0b28ef8986c8b01d0d7f27e69c442e21cdf8e
Pedro <pedro.fjesus@sptech.school>
2025-04-17 10:15:39 -0300

    feat: :fire: mudando proporção de elementos

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -9,17 +9,15 @@ import PerguntasFrequentes from "../components/Index/PerguntasFrequentes";
 export default function HomePage() {
   return (
     <>
-      <header className="absolute top-0 left-0 z-11 w-full h-[80px] flex items-center justify-between text-white px-8">
-        <div className="m-4 flex justify-center items-center bg-[#15171B] h-16 w-[400px]">
-          <nav className="flex gap-8">
+      <header className="absolute top-0 left-0 z-11 w-full h-[80px] flex items-center justify-between text-white px-8 pl-20 pr-20">
+          <nav className="flex gap-8 m-4 flex justify-center items-center bg-[#15171B] h-16 w-[400px] rounded-[6px]">
             <a href="#" className="text-sm hover:underline font-bold text-[14px] cursor-pointer transition-all">Home</a>
             <a href="#" className="text-sm hover:underline font-bold text-[14px] cursor-pointer transition-all">Sobre nós</a>
             <a href="#" className="text-sm hover:underline font-bold text-[14px] cursor-pointer transition-all">Serviços</a>
             <a href="#" className="text-sm hover:underline font-bold text-[14px] cursor-pointer transition-all">Fale conosco</a>
           </nav>
-        </div>
         <img src={logoImage} alt="Logo CaringU" className="h-[50px]" />
-        <div className="flex gap-16 items-center w-[400px] justify-end">
+        <div className="flex gap-16 items-center w-[300px] justify-end">
           <a href="#" className="text-sm font-bold text-[14px] cursor-pointer hover:underline transition-all">Inscreva-se</a>
           <button className="bg-[var(--laranja)] text-white text-sm font-medium px-4 py-2 rounded hover:bg-orange-600 transition-all h-[40px] w-[80px] cursor-pointer">
             Entrar
@@ -87,7 +85,7 @@ export default function HomePage() {
           <Carousel />
         </div>
       </section>
-      <section className="h-screen w-full bg-[var(--cor-secundaria)] flex flex-col items-center justify-center">
+      <section className="h-180 w-full bg-[var(--cor-secundaria)] flex flex-col items-center justify-center">
         <div className="flex flex-col items-center justify-center gap-4 h-1/3">
           <h1 className="text-[48px] font-bold">
             Perguntas Frequentes
@@ -95,7 +93,7 @@ export default function HomePage() {
           <PerguntasFrequentes />
         </div>
       </section>
-      <section className="h-screen w-full bg-[var(--cor-secundaria)] flex flex-col items-center justify-center">
+      <section className="h-180 w-full bg-[var(--cor-secundaria)] flex flex-col items-center justify-center">
         <h1 className="text-[48px] font-bold">
           Conecte-se, treine e evolua com a CaringU!
         </h1>
@@ -143,7 +141,7 @@ export default function HomePage() {
               <label htmlFor="mensagem" className="text-[14px] text-white">
                 *Mensagem
               </label>
-              <input
+              <textarea
                 id="mensagem"
                 type="text"
                 className="px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-[var(--azul-escuro)] bg-white text-black h-[100px]"
@@ -170,7 +168,7 @@ export default function HomePage() {
             </p>
           </div>
         </div>
-        <div className="flex flex-col items-start justify-center h-full w-[200px] gap-5">
+        <div className="flex flex-col items-start justify-center h-full w-[200px] gap-5" id="navegacao">
           <h1 className="text-white text-[20px] font-bold">
             Navegação
           </h1>
