badb30e879cbacc1a1db97dde3816af1e03caaeb
Gustavo Kohatsu <gustavo.kohatsu@sptech.school>
2025-04-26 19:48:12 -0300

    fix: ajustando caminho de importacao do arquivo routes.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import AppRoutes from './Routes';
+import AppRoutes from './routes.jsx';
 import './styles/global.css';
 
 createRoot(document.getElementById('root')).render(
