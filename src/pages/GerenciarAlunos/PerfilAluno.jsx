e1c946eabd83a1bf0939e3066a8c4c45dae4344c
Gustavo-Kohatsu <gustavo.kohatsu@sptech.school>
2025-08-18 21:40:10 -0300

    fix: alteracao da cor de background (cor secundaria)

--- src/pages/GerenciarAlunos/PerfilAluno.jsx
+++ src/pages/GerenciarAlunos/PerfilAluno.jsx
@@ -28,7 +28,7 @@ const PerfilAluno = () => {
     }, [idAluno]);
 
     return (
-        <div className="flex min-h-screen bg-[#fdfbf7]">
+        <div className="flex min-h-screen bg-[var(--cor-secundaria)]">
             <MenuLateral />
             <div className="flex-1 flex flex-col overflow-y-auto">
                 <Header />
