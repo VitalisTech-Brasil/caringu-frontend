d710f4c58fb1b3f3b0629a945c1d2ac29b352101
Igor <igor.jimenez@sptech.school>
2025-09-08 01:16:53 -0300

    fix:"Adjusting the 'CardAluno' for the 'Plano' screen"

--- src/components/Utils/GerenciarAlunos/CardAluno.jsx
+++ src/components/Utils/GerenciarAlunos/CardAluno.jsx
@@ -12,7 +12,13 @@ const CardAluno = ({
     setOpenMenuId,
     imgErro,
     setImgErro,
-    totalCards
+    totalCards,
+    alinhamentoCard = "justify-start",
+    origemUso = "gerenciarAlunos",
+    origemUsoOption = "gerenciarAlunos",
+    heightCardInterno = "55%",
+    heightCardInternoWeb = "50%",
+    gapConteudo = "gap-0",
 }) => {
 
     const menuRef = useRef(null);
@@ -50,21 +56,6 @@ const CardAluno = ({
                     <path d="M21 22H3C2.59 22 2.25 21.66 2.25 21.25C2.25 20.84 2.59 20.5 3 20.5H21C21.41 20.5 21.75 20.84 21.75 21.25C21.75 21.66 21.41 22 21 22Z" fill="#738CAB" />
                     <path d="M19.0201 3.47967C17.0801 1.53967 15.1801 1.48967 13.1901 3.47967L11.9801 4.68967C11.8801 4.78967 11.8401 4.94967 11.8801 5.08967C12.6401 7.73967 14.7601 9.85967 17.4101 10.6197C17.4501 10.6297 17.4901 10.6397 17.5301 10.6397C17.6401 10.6397 17.7401 10.5997 17.8201 10.5197L19.0201 9.30967C20.0101 8.32967 20.4901 7.37967 20.4901 6.41967C20.5001 5.42967 20.0201 4.46967 19.0201 3.47967Z" fill="#738CAB" />
                     <path d="M15.6098 11.5298C15.3198 11.3898 15.0398 11.2498 14.7698 11.0898C14.5498 10.9598 14.3398 10.8198 14.1298 10.6698C13.9598 10.5598 13.7598 10.3998 13.5698 10.2398C13.5498 10.2298 13.4798 10.1698 13.3998 10.0898C13.0698 9.8098 12.6998 9.4498 12.3698 9.0498C12.3398 9.0298 12.2898 8.9598 12.2198 8.8698C12.1198 8.7498 11.9498 8.5498 11.7998 8.3198C11.6798 8.1698 11.5398 7.9498 11.4098 7.7298C11.2498 7.4598 11.1098 7.1898 10.9698 6.9098C10.9486 6.86441 10.9281 6.81924 10.9083 6.77434C10.7607 6.44102 10.3261 6.34358 10.0683 6.60133L4.33983 12.3298C4.20983 12.4598 4.08983 12.7098 4.05983 12.8798L3.51983 16.7098C3.41983 17.3898 3.60983 18.0298 4.02983 18.4598C4.38983 18.8098 4.88983 18.9998 5.42983 18.9998C5.54983 18.9998 5.66983 18.9898 5.78983 18.9698L9.62983 18.4298C9.80983 18.3998 10.0598 18.2798 10.1798 18.1498L15.9011 12.4285C16.1607 12.1689 16.0628 11.7235 15.7252 11.5794C15.6872 11.5632 15.6488 11.5467 15.6098 11.5298Z" fill="#738CAB" />
-                </svg>
-            </button>
-
-
-            <button
-                className="flex items-center justify-between gap-2 p-1 sm:p-2 hover:text-gray-900 hover:bg-gray-100 rounded text-left cursor-pointer"
-                onClick={() => onMenuAction('anamnese', aluno)}
-            >
-                <span className="">
-                    Agendar Aula
-                </span>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                    <path d="M12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22Z" stroke="#15171B" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                    <path d="M8 12H16" stroke="#15171B" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                    <path d="M12 16V8" stroke="#15171B" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
                 </svg>
             </button>
 
@@ -117,11 +108,12 @@ const CardAluno = ({
 
 
     return (
-        <div className={`flex flex-col w-full ${totalCards > 2 ? 'h-full' : 'h-full sm:h-full 2xl:h-1/2'} items-start justify-start bg-[var(--cor-secundaria)] rounded-md sm:p-4 p-2 hover:bg-gray-50 border-2 border-gray-200`}
+        <div 
+            className={`flex flex-col w-full ${totalCards > 2 ? 'h-full' : `h-full sm:h-full 2xl:h-[${heightCardInterno}]`} items-start ${alinhamentoCard} bg-[var(--cor-secundaria)] rounded-md 2xl:p-4 sm:p-3 p-2 hover:bg-gray-50 border-2 border-gray-200 ${gapConteudo}`}
             key={aluno.idAluno}
         >
             <div
-                className="relative flex flex-col-reverse sm:flex-row sm:items-center items-start justify-between  sm:gap-4 w-full h-[55%] sm:h-1/3 gap-3"
+                className={`relative flex flex-col-reverse sm:flex-row sm:items-center items-start justify-between  sm:gap-4 w-full h-[${heightCardInterno}] sm:h-1/3 gap-3`}
             >
                 <div className="gap-1 sm:gap-4 flex sm:flex-row flex-col items-center justify-end w-full h-full">
                     {aluno.urlFotoPerfil && !imgErro ? (
@@ -147,31 +139,33 @@ const CardAluno = ({
                     <div className="text-[var(--azul-claro)] font-normal text-xs bg-[#748CAB36] rounded-[15px] h-auto w-[100px] p-[0.5rem] 2xl:p-2  flex flex-row items-center justify-center text-center">
                         {aluno.nomePlano}
                     </div>
-
-                    {/* Botão + menu */}
-                    <div onClick={(e) => {
-                        e.stopPropagation(); // Prevent card click event
-                        setOpenMenuId(openMenuId === aluno.idAluno ? null : aluno.idAluno);
-                    }}
-                        className="w-8 h-8 pt-2 sm:pt-0 flex justify-center items-center rounded-[5px] cursor-pointer sm:hover:bg-gray-300 transition duration-200">
-                        <div className="relative" ref={buttonRef}>
-                            <button
-                                className="flex items-center justify-center sm:w-6 w-4 sm:h-6 h-4 cursor-pointer"
-                            >
-                                <FaEllipsisV className="text-xl" />
-                            </button>
-
-                            {openMenuId === aluno.idAluno && (
-                                <div
-                                    ref={menuRef}
-                                    onClick={(e) => e.stopPropagation()}
-                                    className="absolute right-0 mt-2 z-[9999] bg-[var(--cor-secundaria)] border-2 border-gray-200 rounded-md shadow-lg p-2 w-40 sm:w-55"
+                    {origemUsoOption === "gerenciarAlunos" && (
+                        //Botão + menu
+                        <div onClick={(e) => {
+                            e.stopPropagation(); // Prevent card click event
+                            setOpenMenuId(openMenuId === aluno.idAluno ? null : aluno.idAluno);
+                        }}
+                            className="w-8 h-8 pt-2 sm:pt-0 flex justify-center items-center rounded-[5px] cursor-pointer sm:hover:bg-gray-300 transition duration-200">
+                            <div className="relative" ref={buttonRef}>
+                                <button
+                                    className="flex items-center justify-center sm:w-6 w-4 sm:h-6 h-4 cursor-pointer"
                                 >
-                                    <AlunoActionsMenu aluno={aluno} />
-                                </div>
-                            )}
+                                    <FaEllipsisV className="text-xl" />
+                                </button>
+
+                                {openMenuId === aluno.idAluno && (
+                                    <div
+                                        ref={menuRef}
+                                        onClick={(e) => e.stopPropagation()}
+                                        className="absolute right-0 mt-2 z-[9999] bg-[var(--cor-secundaria)] border-2 border-gray-200 rounded-md shadow-lg p-2 w-40 sm:w-55"
+                                    >
+                                        <AlunoActionsMenu aluno={aluno} />
+                                    </div>
+                                )}
+                            </div>
                         </div>
-                    </div>
+                    )}
+
                 </div>
             </div>
             <div className="w-full h-[30%] sm:h-1/3 flex flex-col items-start justify-center">
@@ -192,18 +186,20 @@ const CardAluno = ({
                     <span><b>Nível de atividade atual:</b> {formatarNivelAtividade(aluno.nivelAtividade)}</span>
                 </p>
             </div>
-            <div className="w-full h-[15%] sm:h-1/3 flex flex-col items-center sm:justify-end justify-center border-t-2 border-gray-200 rounded-b-md">
-                <Button
-                    id={"btn-visualizar-aluno"}
-                    texto={"Ver Perfil do Aluno"}
-                    cor={"var(--azul-claro)"}
-                    corTexto="#fff"
-                    ariaLabel={"Visualizar Perfil"}
-                    fontWeight={"600"}
-                    classNameExtra={"sm:text-base text-xs sm:h-[50px] h-[30px] sm-w-[80%] w-[90%]"}
-                    onClick={() => onCardClick(aluno.idAluno)}
-                />
-            </div>
+            {origemUso === "gerenciarAlunos" && (
+                <div className="w-full h-[15%] sm:h-1/3 flex flex-col items-center sm:justify-end justify-center border-t-2 border-gray-200 rounded-b-md">
+                    <Button
+                        id={"btn-visualizar-aluno"}
+                        texto={"Ver Perfil do Aluno"}
+                        cor={"var(--azul-claro)"}
+                        corTexto="#fff"
+                        ariaLabel={"Visualizar Perfil"}
+                        fontWeight={"600"}
+                        classNameExtra={"sm:text-base text-xs 2xl:h-[50px] sm:h-[35px] h-[30px] sm-w-[80%] w-[90%]"}
+                        onClick={() => onCardClick(aluno.idAluno)}
+                    />
+                </div>
+            )}
         </div>
     );
 }
