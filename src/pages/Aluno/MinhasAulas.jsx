47602d33e91cd7d11fb7743b1c0d866f386ff4b1
Igor <igor.jimenez@sptech.school>
2025-10-25 13:03:39 -0300

    fix: Reusing the CardAulaTreino component

--- src/pages/Aluno/MinhasAulas.jsx
+++ src/pages/Aluno/MinhasAulas.jsx
@@ -1,13 +1,41 @@
 import MenuLateralAluno from '../../components/Aluno/MenuLateral/MenuLateral';
 import Header from '../../components/Aluno/Header/Header';
-import Pagination from '../../components/Utils/Pagination';
-import Button from "../../components/Utils/Button";
 import { useRef } from 'react';
+import AulaResumoCard from "../../components/Utils/GerenciarAlunos/CardAulaTreino"
 
 function MinhasAulas() {
 
-      const menuRef = useRef(null);
-    
+    const menuRef = useRef(null);
+    const aulas = [
+        { idAula: 1, data_horario_inicio: "2025-12-01 08:00:00", data_horario_fim: "2025-12-01 09:00:00" },
+        { idAula: 2, data_horario_inicio: "2025-12-02 08:00:00", data_horario_fim: "2025-05-02 09:00:00" },
+        { idAula: 3, data_horario_inicio: "2025-12-03 08:00:00", data_horario_fim: "2025-12-03 09:00:00" },
+        { idAula: 4, data_horario_inicio: "2025-12-04 08:00:00", data_horario_fim: "2025-12-04 09:00:00" },
+        { idAula: 5, data_horario_inicio: "2025-12-05 08:00:00", data_horario_fim: "2025-12-05 09:00:00" },
+    ];
+
+    function getDiaSemana(dataString) {
+        const dias = [
+            "Domingo",
+            "Segunda-Feira",
+            "Terça-Feira",
+            "Quarta-Feira",
+            "Quinta-Feira",
+            "Sexta-Feira",
+            "Sábado"
+        ];
+        const data = new Date(dataString.replace(" ", "T"));
+        return dias[data.getDay()];
+    }
+
+    function getDataFormatada(dataString) {
+        const data = new Date(dataString.replace(" ", "T"));
+        const dia = String(data.getDate()).padStart(2, '0');
+        const mes = String(data.getMonth() + 1).padStart(2, '0');
+        const ano = data.getFullYear();
+        return `${dia}/${mes}/${ano}`;
+    }
+
     return (
         <div className="flex min-h-screen bg-[#fdfbf7]">
             <MenuLateralAluno ref={menuRef} />
@@ -47,137 +75,32 @@ function MinhasAulas() {
                         </svg>
                     </div>
                 </div>
-                <div className="w-full h-auto flex flex-col md:flex-row md:flex-wrap">
+                <div className="w-full h-auto grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 px-[1rem]">
                     {/* Card de Evolução */}
-                    <div className="w-[90%] md:w-[45%] lg:w-[30%] mx-auto my-6 p-4 border border-gray-300 rounded-lg flex flex-col gap-4">
-                        <span className="text-[20px] sm:text-[24px] font-bold text-[#1E293B]">
-                            Aula - 10/05/2025
-                        </span>
-                        <div className="flex justify-between items-center">
-                            <div className='flex items-center gap-2 sm:gap-4'>
-                                <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <rect width="17" height="17" fill="#FDFFFD" />
-                                    <path d="M15.5338 4.7883L13.1467 14.3721C12.9767 15.0875 12.3392 15.5833 11.6025 15.5833H2.29501C1.22543 15.5833 0.460438 14.5349 0.779188 13.5078L3.76127 3.93124C3.96668 3.26541 4.58294 2.80493 5.27711 2.80493H13.9896C14.6625 2.80493 15.2221 3.21577 15.4559 3.78244C15.5904 4.08702 15.6188 4.43413 15.5338 4.7883Z" stroke="#1D2D44" stroke-miterlimit="10" />
-                                    <path d="M11.3334 15.5833H14.7192C15.633 15.5833 16.3484 14.8112 16.2846 13.8975L15.5834 4.25" stroke="#1D2D44" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
-                                    <path d="M6.85669 4.51919L7.59336 1.45923" stroke="#1D2D44" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
-                                    <path d="M11.6025 4.52629L12.2684 1.45215" stroke="#1D2D44" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
-                                    <path d="M5.45422 8.5H11.1209" stroke="#1D2D44" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
-                                    <path d="M4.74585 11.3333H10.4125" stroke="#1D2D44" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
-                                </svg>
-                                <span className='text-[16px] sm:text-[20px] text-[#1E293B]'>
-                                    Segunda-Feira
-                                </span>
-                            </div>
-                            <div className='flex items-center gap-2 sm:gap-4'>
-                                <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M8.50002 1.41675C4.5971 1.41675 1.41669 4.59716 1.41669 8.50008C1.41669 12.403 4.5971 15.5834 8.50002 15.5834C12.4029 15.5834 15.5834 12.403 15.5834 8.50008C15.5834 4.59716 12.4029 1.41675 8.50002 1.41675ZM11.5813 11.0288C11.4821 11.1988 11.305 11.2909 11.1209 11.2909C11.0288 11.2909 10.9367 11.2697 10.8517 11.213L8.65585 9.90258C8.11044 9.57675 7.70669 8.86133 7.70669 8.23091V5.32675C7.70669 5.03633 7.94752 4.7955 8.23794 4.7955C8.52835 4.7955 8.76919 5.03633 8.76919 5.32675V8.23091C8.76919 8.48591 8.98169 8.86133 9.20127 8.98883L11.3971 10.2992C11.6521 10.448 11.7371 10.7738 11.5813 11.0288Z" fill="#1D2D44" />
-                                </svg>
-
-                                <span className="text-[16px] sm:text-[20px] text-[#1E293B]">
-                                    15:00 - 16:00
-                                </span>
-                            </div>
+                    {aulas.length === 0 ? (
+                        <div className="text-sm italic text-gray-500">
+                            Nenhum treino atribuído.
                         </div>
-                        <div className='flex items-center justify-center'>
-
-                            <Button
-                                texto="Ver Treinos"
-                                fontSize="14px"
-                                fontWeight="600"
-                                width="80%"
-                                height="45px"
-                                cor="#748CAB"
-                                corTexto="#FFFFFF"
-                                classNameExtra='ml-7 sm:ml-0'
+                    ) : (
+                        aulas.map(a => (
+                            <AulaResumoCard
+                                key={a.idAula}
+                                data={getDataFormatada(a.data_horario_inicio)}
+                                diaSemana={getDiaSemana(a.data_horario_inicio)}
+                                horarioInicio={a.data_horario_inicio.slice(11, 16)}
+                                horarioFim={a.data_horario_fim.slice(11, 16)}
+                                paddingCard="p-4"
+                                alignIcons="flex-row"
+                                alignText="justify-start"
+                                onVerTreinos={() => {
+                                    setAulaSelecionada(a);
+                                    fetchInfosAulasFeedback(a.idAula);
+                                }}
                             />
-                        </div>
-                    </div>
-                    <div className="w-[90%] md:w-[45%] lg:w-[30%] mx-auto my-6 p-4 border border-gray-300 rounded-lg flex flex-col gap-4">
-                        <span className="text-[20px] sm:text-[24px] font-bold text-[#1E293B]">
-                            Aula - 10/05/2025
-                        </span>
-                        <div className="flex justify-between items-center">
-                            <div className='flex items-center gap-2 sm:gap-4'>
-                                <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <rect width="17" height="17" fill="#FDFFFD" />
-                                    <path d="M15.5338 4.7883L13.1467 14.3721C12.9767 15.0875 12.3392 15.5833 11.6025 15.5833H2.29501C1.22543 15.5833 0.460438 14.5349 0.779188 13.5078L3.76127 3.93124C3.96668 3.26541 4.58294 2.80493 5.27711 2.80493H13.9896C14.6625 2.80493 15.2221 3.21577 15.4559 3.78244C15.5904 4.08702 15.6188 4.43413 15.5338 4.7883Z" stroke="#1D2D44" stroke-miterlimit="10" />
-                                    <path d="M11.3334 15.5833H14.7192C15.633 15.5833 16.3484 14.8112 16.2846 13.8975L15.5834 4.25" stroke="#1D2D44" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
-                                    <path d="M6.85669 4.51919L7.59336 1.45923" stroke="#1D2D44" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
-                                    <path d="M11.6025 4.52629L12.2684 1.45215" stroke="#1D2D44" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
-                                    <path d="M5.45422 8.5H11.1209" stroke="#1D2D44" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
-                                    <path d="M4.74585 11.3333H10.4125" stroke="#1D2D44" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
-                                </svg>
-                                <span className='text-[16px] sm:text-[20px] text-[#1E293B]'>
-                                    Segunda-Feira
-                                </span>
-                            </div>
-                            <div className='flex items-center gap-2 sm:gap-4'>
-                                <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M8.50002 1.41675C4.5971 1.41675 1.41669 4.59716 1.41669 8.50008C1.41669 12.403 4.5971 15.5834 8.50002 15.5834C12.4029 15.5834 15.5834 12.403 15.5834 8.50008C15.5834 4.59716 12.4029 1.41675 8.50002 1.41675ZM11.5813 11.0288C11.4821 11.1988 11.305 11.2909 11.1209 11.2909C11.0288 11.2909 10.9367 11.2697 10.8517 11.213L8.65585 9.90258C8.11044 9.57675 7.70669 8.86133 7.70669 8.23091V5.32675C7.70669 5.03633 7.94752 4.7955 8.23794 4.7955C8.52835 4.7955 8.76919 5.03633 8.76919 5.32675V8.23091C8.76919 8.48591 8.98169 8.86133 9.20127 8.98883L11.3971 10.2992C11.6521 10.448 11.7371 10.7738 11.5813 11.0288Z" fill="#1D2D44" />
-                                </svg>
-
-                                <span className="text-[16px] sm:text-[20px] text-[#1E293B]">
-                                    15:00 - 16:00
-                                </span>
-                            </div>
-                        </div>
-                        <div className='flex items-center justify-center'>
-
-                            <Button
-                                texto="Ver Treinos"
-                                fontSize="14px"
-                                fontWeight="600"
-                                width="80%"
-                                height="45px"
-                                cor="#748CAB"
-                                corTexto="#FFFFFF"
-                                classNameExtra='ml-7 sm:ml-0'
-                            />
-                        </div>
-                    </div>
-                    <div className="w-[90%] md:w-[45%] lg:w-[30%] mx-auto my-6 p-4 border border-gray-300 rounded-lg flex flex-col gap-4">
-                        <span className="text-[20px] sm:text-[24px] font-bold text-[#1E293B]">
-                            Aula - 10/05/2025
-                        </span>
-                        <div className="flex justify-between items-center">
-                            <div className='flex items-center gap-2 sm:gap-4'>
-                                <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <rect width="17" height="17" fill="#FDFFFD" />
-                                    <path d="M15.5338 4.7883L13.1467 14.3721C12.9767 15.0875 12.3392 15.5833 11.6025 15.5833H2.29501C1.22543 15.5833 0.460438 14.5349 0.779188 13.5078L3.76127 3.93124C3.96668 3.26541 4.58294 2.80493 5.27711 2.80493H13.9896C14.6625 2.80493 15.2221 3.21577 15.4559 3.78244C15.5904 4.08702 15.6188 4.43413 15.5338 4.7883Z" stroke="#1D2D44" stroke-miterlimit="10" />
-                                    <path d="M11.3334 15.5833H14.7192C15.633 15.5833 16.3484 14.8112 16.2846 13.8975L15.5834 4.25" stroke="#1D2D44" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
-                                    <path d="M6.85669 4.51919L7.59336 1.45923" stroke="#1D2D44" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
-                                    <path d="M11.6025 4.52629L12.2684 1.45215" stroke="#1D2D44" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
-                                    <path d="M5.45422 8.5H11.1209" stroke="#1D2D44" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
-                                    <path d="M4.74585 11.3333H10.4125" stroke="#1D2D44" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
-                                </svg>
-                                <span className='text-[16px] sm:text-[20px] text-[#1E293B]'>
-                                    Segunda-Feira
-                                </span>
-                            </div>
-                            <div className='flex items-center gap-2 sm:gap-4'>
-                                <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M8.50002 1.41675C4.5971 1.41675 1.41669 4.59716 1.41669 8.50008C1.41669 12.403 4.5971 15.5834 8.50002 15.5834C12.4029 15.5834 15.5834 12.403 15.5834 8.50008C15.5834 4.59716 12.4029 1.41675 8.50002 1.41675ZM11.5813 11.0288C11.4821 11.1988 11.305 11.2909 11.1209 11.2909C11.0288 11.2909 10.9367 11.2697 10.8517 11.213L8.65585 9.90258C8.11044 9.57675 7.70669 8.86133 7.70669 8.23091V5.32675C7.70669 5.03633 7.94752 4.7955 8.23794 4.7955C8.52835 4.7955 8.76919 5.03633 8.76919 5.32675V8.23091C8.76919 8.48591 8.98169 8.86133 9.20127 8.98883L11.3971 10.2992C11.6521 10.448 11.7371 10.7738 11.5813 11.0288Z" fill="#1D2D44" />
-                                </svg>
-
-                                <span className="text-[16px] sm:text-[20px] text-[#1E293B]">
-                                    15:00 - 16:00
-                                </span>
-                            </div>
-                        </div>
-                        <div className='flex items-center justify-center'>
-
-                            <Button
-                                texto="Ver Treinos"
-                                fontSize="14px"
-                                fontWeight="600"
-                                width="80%"
-                                height="45px"
-                                cor="#748CAB"
-                                corTexto="#FFFFFF"
-                                classNameExtra='ml-7 sm:ml-0'
-                            />
-                        </div>
-                    </div>
+                        ))
+                    )}
+                    
+                   
                 </div>
                 {/* Arrumar aqui Depois */}
                 {/* <Pagination
