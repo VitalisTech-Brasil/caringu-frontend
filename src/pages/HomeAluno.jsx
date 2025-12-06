cfcc9097153e79765892187a92d65ccd387b87dc
Igor <igor.jimenez@sptech.school>
2025-12-06 19:02:09 -0300

    fix: adjusting props

--- src/pages/HomeAluno.jsx
+++ src/pages/HomeAluno.jsx
@@ -475,7 +475,7 @@ const HomeAluno = () => {
                         fontSize={"16px"}
                         classNameExtra="px-4 py-1"
                         onClick={() => navigate(
-                          `/acompanhar-aula-aluno/${aula.idAula}`,
+                          `/acompanhar-aula-aluno/${aula.id}`,
                           { state: { compromisso: { ...aula, status: "compromisso" } } }
                         )}
 
@@ -549,18 +549,18 @@ const HomeAluno = () => {
                           <span>Personal Trainer</span>
                         </div>
                         <div className="flex flex-row items-center justify-start gap-4 text-[#15171B87] text-sm font-medium border border-gray-300 py-1.5 pl-4 rounded-[6px] w-full bg-[var(--cor-secundaria)] h-auto">
-                          {aula.urlFotoPerfil && !errosImagem[aula.urlFotoPerfil] ? (
+                          {aula.personal.urlFotoPerfil && !errosImagem[aula.personal.urlFotoPerfil] ? (
                             <img
-                              src={aula.urlFotoPerfil}
-                              alt={aula.nomePersonal}
+                              src={aula.personal.urlFotoPerfil}
+                              alt={aula.personal.nome}
                               className="w-[45px] h-[45px] object-cover rounded"
-                              onError={() => lidarErroImagem(aula.urlFotoPerfil)}
+                              onError={() => lidarErroImagem(aula.personal.urlFotoPerfil)}
                             />
                           ) : (
                             <FaUserCircle className="flex-shrink-0 w-[45px] h-[45px]" />
                           )}
                           <span>
-                            {aula.nomePersonal}
+                            {aula.personal.nome}
                           </span>
                         </div>
                       </div>
